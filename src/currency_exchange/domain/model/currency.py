c5630f47c62d1aec86ffc5d36da1199d771b4231
TheEmpressDiadema <mauhkpaffftep@gmail.com>
2026-05-06 00:28:30 +0300

    BREAKING CHANGE: restructured some model folders and files

--- src/currency_exchange/domain/model/currency.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-
-@dataclass
-class Currency:
-    id: int
-    code: str
-    full_name: str
-    sign: str
--- src/currency_exchange/domain/model/exchange_rate.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-
-@dataclass
-class ExchangeRate:
-    id: int
-    base_currency_id: int
-    target_currency_id: int
-    rate: float
