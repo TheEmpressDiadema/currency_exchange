42d82002b8f77de8b0402abcb3634328fd615d02
TheEmpressDiadema <mauhkpaffftep@gmail.com>
2026-05-06 00:25:52 +0300

    feat: some testing code

--- src/currency_exchange/__init__.py
+++ src/currency_exchange/__init__.py
@@ -1,2 +1,13 @@
+from currency_exchange.model.DAO.currencyDAO import CurrencyDAO
+from currency_exchange.model.DAO.exchangeRateDAO import ExchangeRateDAO
+
 def main() -> None:
-    pass
+    cur_dao = CurrencyDAO()
+    exchange_rate_dao = ExchangeRateDAO()
+    print(cur_dao.get_by_id(1))
+    print(cur_dao.get_all())
+    print(cur_dao.get_by_code("EUR"))
+    print("здесь будут эксченжрэйт")
+    print(exchange_rate_dao.get_all())
+    print(exchange_rate_dao.get_by_id(1))
+    print(exchange_rate_dao.get_by_code_pair(("EUR", "USD")))
